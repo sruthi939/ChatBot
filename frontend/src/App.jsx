e9fde84d47de1a1f0b527e8a4bc33d46333d8bd1
sruthi939 <sathiyasuru@gmail.com>
2026-04-28 22:03:52 +0530

    create frontend structure with  pages, components, and routing setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-gray-500">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </div>
   )
 }
 
